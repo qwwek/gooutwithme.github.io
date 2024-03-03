5cbbaf86f41d2ddf718c93a5c40eccd9309b24f3
Tymofii Chernata <timofeychernata@gmail.com>
2024-03-03 18:02:28 +0100

    templates and login endpoint WIP

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template, request
 import weather
 
 app = Flask(__name__)
@@ -6,5 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     forecast = weather.getforecast('Oslo')
-    html = '%s\n<p>Hello, World!</p>' % forecast
-    return html
+    return render_template('index.html', data="123")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if len(email) == 0 or len(password) == 0:
+            return render_template('error.html')
+        
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
